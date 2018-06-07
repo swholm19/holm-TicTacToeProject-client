c617e9c3f112019b2d217f6bdc5946766ec23b9f
swholm19 <swholm19@gmail.com>
2018-06-07 10:57:12 -0400

    Updated config.js file for new urls
    -changed Dev and Prod urls to match documentation
    -NPM install updated the package-lock.json

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,9 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
-  development: 'http://localhost:4741'
+  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  development: 'http://tic-tac-toe.wdibos.com'
+  // development: 'http://localhost:4741'
 }
 
 if (window.location.hostname === 'localhost') {
