e5387eed93c5206089ec3070cf55f65995ba034a
swholm19 <swholm19@gmail.com>
2018-06-12 13:06:22 -0400

    Updated spacing in code to make easier to read

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -21,6 +21,7 @@ $(() => {
   // The below line of code is event handlers to listen if the gameboard
   // has been clicked.
   $('.gamebox').on('click', boardEvents.onPlayerMove)
+
   $('#resetGame').on('click', boardEvents.onReset)
 
   $('#playerHistory').on('click', historyEvents.onUpdatePlayerHistory)
